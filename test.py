c93b073073d2afdd796874bc9d5110961cd867be
andritanuww <57549600+Andritanu@users.noreply.github.com>
2021-12-23 11:52:27 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ page = requests.get('https://www.worldometers.info/coronavirus/country/indonesia
 
 after_bs = BeautifulSoup(page.content, 'html.parser') 
 
-find_data = after_bs.find_all(id='maincounter-wrap')  #ngambiliddarielements
+find_data = after_bs.find_all(id='maincounter-wrap')  #getFromElements
 
 #text = find_data.text
 
